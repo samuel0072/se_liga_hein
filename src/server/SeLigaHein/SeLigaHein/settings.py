6e772b202265b4044364cb7d95be2ae850b4281a
Samuel Lima <30638348+samuel0072@users.noreply.github.com>
2021-09-25 08:43:02 -0300

    :fire: Remove pasta de migrações

--- src/server/SeLigaHein/SeLigaHein/settings.py
+++ src/server/SeLigaHein/SeLigaHein/settings.py
@@ -32,7 +32,7 @@ AUTH_USER_MODEL = 'usuarios.Usuario'
 # Application definition
 
 INSTALLED_APPS = [
-    'usuarios.apps.UsuariosConfig'
+    'usuarios.apps.UsuariosConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
