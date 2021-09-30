f976b9fc3f6a29e790e6c8fee0e993b0f8405d55
Samuel Lima <sls@ic.ufal.br>
2021-09-30 10:39:11 -0300

    :construction: Adiciona models de endereço
    Modificado estrutura do banco de dados
    
    futuramente imagem vai ser atualizada

--- src/server/SeLigaHein/vagas/admin.py
+++ src/server/SeLigaHein/vagas/admin.py
@@ -3,3 +3,6 @@ from .models import *
 # Register your models here.
 
 admin.site.register(Usuario)
+admin.site.register(Vaga)
+admin.site.register(Endereco)
+admin.site.register(EntidadeEndereco)
--- src/server/SeLigaHein/vagas/models.py
+++ src/server/SeLigaHein/vagas/models.py
@@ -16,11 +16,11 @@ class Usuario(AbstractUser):
     ]
 
     tipo = models.CharField(max_length=3, choices=TIPOS_USUARIOS, default=CANDIDATO)#tipo do usuário, 0 para candidato e 1 para empresa
-    email_2 = models.CharField(max_length=255)#o django já possui um campo de email
+    email_2 = models.CharField(max_length=255, null=True, blank=True)#o django já possui um campo de email
     telefone_1 = models.CharField(max_length=255)
-    telefone_2 = models.CharField(max_length=255)
-    descricao = models.TextField()#campo de bio ou descrição de uma empresa
-    cnpj = models.CharField(max_length=14, default = '')#campo opcional para usuários do tipo empresa
+    telefone_2 = models.CharField(max_length=255, null=True, blank=True)
+    descricao = models.TextField(null=True, blank=True)#campo de bio ou descrição de uma empresa
+    cnpj = models.CharField(max_length=14, default = '', null=True, blank=True)#campo opcional para usuários do tipo empresa
 
     def nome_completo(self):
         """
@@ -28,9 +28,13 @@ class Usuario(AbstractUser):
         """
         return "{0} {1}".format(self.first_name, self.last_name)
 
+    def __str__(self):
+        return "Usuário {0}: {1}".format(self.nome_completo(), self.get_tipo_display())
+
 class TimeStampedModel(models.Model):
     """
-    Essa classe contém campos de timestamps
+    Essa classe contém campos de timestamps.
+
     Todos os models com exceção de Usuario vão
     herdar dessa classe.
     """
@@ -40,8 +44,45 @@ class TimeStampedModel(models.Model):
     class Meta:
         abstract = True
 
-class Vagas(TimeStampedModel):
+
+class EntidadeEndereco(TimeStampedModel):
+    """
+    Model de entidade de um endereço.
+
+    Esse model pode representar um país, estado ou cidade.
+    """
+    PAIS = 'PS'
+    ESTADO = 'ET'
+    CIDADE = 'CD'
+    TIPOS_CIDADE = [
+        (PAIS, 'País'),
+        (ESTADO, 'Estado'),
+        (CIDADE, 'Cidade')
+    ]
+    nome = models.CharField(max_length=255)
+    sigla = models.CharField(max_length=3)
+    tipo = models.CharField(choices=TIPOS_CIDADE, default=PAIS, max_length=2)
+
+    def __str__(self):
+        return "Entidade Endereço: {0}-{1} do tipo {2}".format(self.nome, self.sigla, self.get_tipo_display())
+
+class Endereco(TimeStampedModel):
+    """
+    Model de endereço que contém um país, um estado e uma cidade.
+    """
+    pais = models.ForeignKey(EntidadeEndereco, on_delete=models.PROTECT, related_name='pais')
+    cidade = models.ForeignKey(EntidadeEndereco, on_delete=models.PROTECT, related_name='cidade')
+    estado = models.ForeignKey(EntidadeEndereco, on_delete=models.PROTECT, related_name='estado')
+
+    def __str__(self):
+        return "Endereço: {0}, {1} de {2}".format(self.pais.nome, self.cidade.nome, self.estado.nome)
+
+class Vaga(TimeStampedModel):
+    """
+    Model de vagas. Todas as vagas do sistema são instância desse model
+
+    """
     descricao = models.TextField()#campo de descrição da vaga
-    data_inicio = models.DateTimeField()
+    data_inicio = models.DateTimeField(auto_now_add=True)
     data_fim = models.DateTimeField()
-    
+    endereco = models.ForeignKey(Endereco, on_delete=models.PROTECT, null=True, blank=True)
