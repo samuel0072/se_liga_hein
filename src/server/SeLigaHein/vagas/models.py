7eb2b9fbbeb6135fe04cf7833037e84c54684cd5
Samuel Lima <sls@ic.ufal.br>
2021-09-30 09:38:13 -0300

    $:construction: Cria model de vagas\nAdiciona também model para campos de timestamps

--- src/server/SeLigaHein/vagas/models.py
+++ src/server/SeLigaHein/vagas/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
+
+
 class Usuario(AbstractUser):
     """
     Modelo do usuário padrão do sistema
@@ -26,4 +28,20 @@ class Usuario(AbstractUser):
         """
         return "{0} {1}".format(self.first_name, self.last_name)
 
+class TimeStampedModel(models.Model):
+    """
+    Essa classe contém campos de timestamps
+    Todos os models com exceção de Usuario vão
+    herdar dessa classe.
+    """
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Vagas(TimeStampedModel):
+    descricao = models.TextField()#campo de descrição da vaga
+    data_inicio = models.DateTimeField()
+    data_fim = models.DateTimeField()
     
