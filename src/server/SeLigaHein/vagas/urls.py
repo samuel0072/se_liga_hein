374133f9ca291d7fbc2a3348efbfb9f66f5c9e04
Samuel Lima <sls@ic.ufal.br>
2021-10-07 10:52:29 -0300

    :construction: Adiciona implementações iniciais de rotas

--- src/server/SeLigaHein/vagas/urls.py
+++ src/server/SeLigaHein/vagas/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
+from vagas import views
 
 urlpatterns = [
+    path('', views.vaga, name = 'index'),
     path('api-auth/', include('rest_framework.urls')),
+    path('listar/', views.listar, name = 'listar'),
 ]
--- src/server/SeLigaHein/vagas/views.py
+++ src/server/SeLigaHein/vagas/views.py
@@ -1,3 +1,44 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import *
+from .models import *
 
 # Create your views here.
+content_type = "aplication/json"
+
+@api_view(['GET'])
+def listar(request):
+    #TODO: Adicionar filtros listado em docs/README.md
+    vagas = Vaga.objects.all()
+    serializer = VagaSerializer(vagas, many = True)
+    return Response(serializer.data, content_type=content_type)
+
+@api_view(['GET', 'POST', 'PUT', 'DELETE'])
+def vaga(request):
+    
+    if request.method == 'GET':
+        
+        id = request.query_params["id"]
+        vaga = Vaga.objects.get(pk=id)
+        serializer = VagaSerializer(vaga)
+        
+        return Response(serializer.data, content_type=content_type)
+            
+    if request.method == 'POST':
+        serializer = VagaSerializer(data = request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, content_type=content_type, status = status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        pass
